3b83e62e75a2cb42b0ad9f21c1e29e421668a9e0
Vishal Waghela <103985192+vishalwaghela02@users.noreply.github.com>
2023-01-15 23:51:42 +0530

    Add files via upload

---
+++ Array.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main()
+{
+    
+    auto int Arr[5];
+    register int iCnt = 0;
+
+    printf("Enter elements : \n");
+
+   for(iCnt = 0; iCnt < 5; iCnt++ )
+   {
+        scanf("%d",&Arr[iCnt]);
+   }
+
+
+    printf("Elements of array are : \n");
+
+    for(iCnt = 0; iCnt < 5; iCnt++)
+    {
+        printf("%d\n",Arr[iCnt]);
+    }
+
+    return 0;
+}
